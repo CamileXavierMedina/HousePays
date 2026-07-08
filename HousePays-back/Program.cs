946f24eb2663b758818f33318e5c338d3f9c6562
Camile Xavier Medina <camile.medina25@gmail.com>
2026-07-07 22:55:28 -0300

    feat: criação de codigo da program.cs

---
+++ HousePays-back/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using HousePays.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//configura o BD sqlite 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=housepays.db"));
+
+//coonfig. o cors pro react acessar a api
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")//porta que o react roda
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+//ativa allow do cors
+app.UseCors("AllowReact");
+
+//garante q criará as tabelas do BD automaticamente
+using(var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
