1d20330a418b292acb1720b9bfff065dcd5be98b
Camile Xavier Medina <camile.medina25@gmail.com>
2026-07-08 00:54:49 -0300

    refactor: adição de metodos no program.cs

--- HousePays-back/Program.cs
+++ HousePays-back/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using HousePays.Data;
+using HousePays.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,7 +31,53 @@ using(var scope = app.Services.CreateScope())
     db.Database.EnsureCreated();
 }
 
-//endpoint de teste
+//endpoint de teste incial
 app.MapGet("/", () => "Api housepays ativa, configurada no sqlite e pronta prs rotas!");
+
+//ROTA DE PESSOAS//
+
+//lista as pessoas
+app.MapGet("/pessoas", async (AppDbContext db) =>
+{
+    var pessoas = await db.Pessoas.ToListAsync();
+});
+
+//cadastra alguem novo
+app.MapPost("/pessoas", async (Pessoa novaPessoa, AppDbContext db) =>
+{
+    if (string.IsNullOrWhiteSpace(novaPessoa.Nome))
+    {
+        return Results.BadRequest("O nome da pessoa é obrigatorio!");
+    }
+
+    if (novaPessoa.Idade < 0)
+    {
+        return Results.BadRequest("A idade nao pode ser negativa");
+    }
+
+    //id ja vem no guid.newguid 
+    db.Pessoas.Add(novaPessoa);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/pessoas/{novaPessoa.Id}", novaPessoa);
+
+});
+
+//excluir pessoa(exclusao cascata)
+app.MapDelete("/pessoas/{id:guid}", async (Guid id, AppDbContext db) =>
+{
+    var pessoa = await db.Pessoas.FindAsync(id);
+
+    if (pessoa == null)
+    {
+        return Results.NotFound("Pessoa nao identificada!");
+    }
+
+    //entende q config. a exclusao cascata e ja  remove transação sozinho
+    db.Pessoas.Remove(pessoa);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
