166e4752a805668b4b62920baac37ac3b9cc7427
Camile Xavier Medina <camile.medina25@gmail.com>
2026-07-09 08:49:20 -0300

    feat: migra o DbContext para a pasta Dados e atualiza namespaces

---
+++ HousePays-back/Dados/AppDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using HousePays.Modelos;
+
+namespace HousePays.Dados
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas => Set<Pessoa>();
+        public DbSet<Transacao> Transacoes => Set<Transacao>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuração do relacionamento 1 - N com Cascade Delete
+            modelBuilder.Entity<Transacao>()
+                .HasOne(t => t.Pessoa)
+                .WithMany(p => p.Transacoes)
+                .HasForeignKey(t => t.PessoaId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
